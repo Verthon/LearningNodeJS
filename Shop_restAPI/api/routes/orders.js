7b0048f5c4bee54c4ebde7a70b83db7e9db04f17
Verthon <krzysiekksordyl@gmail.com>
2019-06-15 22:06:02 +0200

    Added simple POST, GET, DELETE request for orders

--- Shop_restAPI/api/routes/orders.js
+++ Shop_restAPI/api/routes/orders.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
+  Order.find()
+  .select('productId quantity')
+  .then(doc => {
+    res.status(200).json({doc})
+  })
+  .catch(err => {
+    return {
+      error: err
+    }
+  })
   const order = {
     productId: req.body.productId,
     quantity: req.body.quantity,
@@ -11,5 +24,43 @@ router.get('/', (req, res, next) => {
     order: order,
   })
 });
+
+router.post('/', (req, res, next) => {
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId,
+  });
+  order.save()
+  .then(result => {
+    res.status(201).json({
+      message: 'Order added.',
+      order: result,
+    });
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    })
+  })
+});
+
+router.delete('/:orderId', (req, res, next) => {
+  const id = req.params.orderId
+  Order.deleteOne({ _id: id })
+    .then(result => {
+      res.status(200).json({ 
+        message: "Order deleted.",
+        request: {
+          type: 'POST',
+          url: "http://localhost:3000/orders",
+          body: {quantity: "Number", productId:"String"}
+        } 
+      })
+    })
+    .catch(err => {
+      res.status(500).json({ error: err })
+    })
+})
 
 module.exports = router;
--- Shop_restAPI/api/routes/products.js
+++ Shop_restAPI/api/routes/products.js
@@ -6,9 +6,23 @@ const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id')
     .then(docs => {
-      console.log(docs)
-      res.status(200).json(docs)
+      const response = {
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + doc._id,
+            },
+          }
+        }),
+      }
+      res.status(200).json(response)
     })
     .catch(err => {
       console.log(err)
@@ -25,9 +39,17 @@ router.post('/', (req, res, next) => {
 
   product.save()
     .then(result => {
-      console.log(result)
       res.status(201).json({
-        createdProduct: result,
+        message: 'Product created successfully',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/' + result._id,
+          },
+        },
       })
     })
     .catch(err => {
@@ -41,10 +63,16 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId
   Product.findById(id)
+    .select('name price _id')
     .then(doc => {
-      console.log(doc)
       if (doc) {
-        res.status(200).json(doc)
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products',
+          },
+        })
       } else {
         res.status(404).json({ message: 'Not found product with provided id' })
       }
@@ -56,20 +84,22 @@ router.get('/:productId', (req, res, next) => {
 })
 
 router.patch('/', (req, res, next) => {
-  const id = req.params.productId;
-  const options = {};
+  const id = req.params.productId
+  const options = {}
 
-  for (const ops of req.body){
-    options[ops.propName] = ops.value;
+  for (const ops of req.body) {
+    options[ops.propName] = ops.value
   }
-  Product.patch({ _id: id }, {$set: {options}})
-    .then( res => {
-      console.log(res);
-      res.status(200).json({message: res});
+  Product.patch({ _id: id }, { $set: { options } })
+    .then(res => {
+      res.status(200).json({ 
+        message: "Product updated",
+        product: {res} 
+      })
     })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({error: err})
+      console.log(err)
+      res.status(500).json({ error: err })
     })
 })
 
@@ -77,7 +107,14 @@ router.delete('/:productId', (req, res, next) => {
   const id = req.params.productId
   Product.deleteOne({ _id: id })
     .then(result => {
-      res.status(200).json({ message: result })
+      res.status(200).json({ 
+        message: "Product deleted.",
+        request: {
+          type: 'POST',
+          url: "http://localhost:3000/products",
+          body: {name: "String", price:"Number"}
+        } 
+      })
     })
     .catch(err => {
       res.status(500).json({ error: err })
