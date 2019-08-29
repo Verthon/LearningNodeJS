d7686bf0fc06a3e95739ac99e298b00b6a87a7ff
Verthon <krzysiekksordyl@gmail.com>
2019-08-29 22:47:53 +0200

    Added new mothods to Blockchain class

--- Simple_blockchain/Blockchain.js
+++ Simple_blockchain/Blockchain.js
@@ -1,11 +1,23 @@
+import Block from './Block';
+
 class Blockchain {
   constructor () {
-    this.chain = [];
+    this.chain = [this.createGenesisBlock()];
   }
 
   createGenesisBlock = () => new Block('0', '24/05/2019', 'Genesis Block', '0');
 
   getLatestBlock = () => this.chain[this.chain.length - 1];
 
-  createBlock = () => 
+  addBlock = newBlock => {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = Block.prototype.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid = () => {
+    for( let i = 0; i < this.chain.length - 1; i++ ){
+
+    }
+  }
 }
