6f820334e8fbd45f06405a993999657fbf5d1a0e
Verthon <krzysiekksordyl@gmail.com>
2019-09-05 18:06:18 +0200

    Added webpack config for react, added also ant design

---
+++ MERN_app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react';
+import PropTypes from 'prop-types';
+
+const App = () => {
+  return (
+    <h2>Hello from React app</h2>
+  );
+};
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
