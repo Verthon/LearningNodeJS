a6b6239f57bc7246d03799619e5ddfab4c0f195e
Verthon <krzysiekksordyl@gmail.com>
2019-12-07 17:53:13 +0100

    LOL, forgot to add .gitignore at the init commit. Added working knex setup. Added test environment for postgresql.

--- KoaFarm/knexfile.js
+++ KoaFarm/knexfile.js
@@ -1,44 +1,13 @@
 // Update with your config settings.
-
-module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+require('dotenv').config()
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   }
+})
 
-};
+module.exports = knex
--- KoaFarm/routes/farmers.js
+++ KoaFarm/routes/farmers.js
@@ -1,11 +1,18 @@
 const Router = require('koa-router')
 const router = new Router()
+const db = require('../knexfile.js')
 
 router.get('/api/v1/farmers', async ctx => {
-  ctx.body = {
-    status: '200 success',
-    message: 'farmers route'
-  }
+  console.log('DB object: ', db)
+  return db
+    .select()
+    .from('farmers')
+    .then(data => {
+      ctx.body = {
+        status: '200 success',
+        data: data
+      }
+    })
 })
 
 module.exports = router
